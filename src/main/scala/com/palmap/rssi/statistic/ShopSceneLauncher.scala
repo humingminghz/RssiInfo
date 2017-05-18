df2d406daee79b70e949ce1dd58d878f81f8dbda
shuai.yang <shuai.yang@palmaplus.com>
2017-05-18 18:01:43 +0800

    减少log显示

--- src/main/scala/com/palmap/rssi/statistic/ShopSceneLauncher.scala
+++ src/main/scala/com/palmap/rssi/statistic/ShopSceneLauncher.scala
@@ -5,6 +5,7 @@ import com.palmap.rssi.common.{Common, GeneralMethods}
 import com.palmap.rssi.funcs.{HistoryFuncs, RealTimeFuncs, VisitedFuncs}
 import com.palmap.rssi.message.ShopStore.Visitor
 import kafka.serializer.{DefaultDecoder, StringDecoder}
+import org.apache.log4j.{Level, Logger}
 import org.apache.spark.streaming.kafka.KafkaUtils
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 import org.apache.spark.{SparkConf, SparkContext}
@@ -17,6 +18,8 @@ object ShopSceneLauncher {
 
   def main(args: Array[String]): Unit = {
 
+    Logger.getLogger("org").setLevel(Level.OFF)
+
     val sparkRssiInfoXml = GeneralMethods.getConf(Common.SPARK_CONFIG)
     val broker_list = sparkRssiInfoXml(Common.KAFKA_METADATA_BROKER)
     val group_id = sparkRssiInfoXml(Common.SPARK_GROUP_ID)
@@ -25,7 +28,7 @@ object ShopSceneLauncher {
     System.setProperty("spark.storage.memoryFraction", "0.4")
     System.setProperty("spark.shuffle.io.preferDirectBufs", "false")
 
-    val sparkConf = new SparkConf().setAppName("frost-launcher")
+    val sparkConf = new SparkConf().setAppName("frost-launcher").setMaster("local[*]")
     sparkConf.set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
     sparkConf.set("spark.shuffle.consolidateFiles", "true") // 开启shuffle block file的合并
     sparkConf.registerKryoClasses(Array(classOf[Visitor], classOf[Visitor.Builder]))
@@ -40,13 +43,14 @@ object ShopSceneLauncher {
     messagesRdd.count().map(x => "Received " + x + " kafka events.").print()
 
     val visitorRdd = messagesRdd.map(_._2)
-      .flatMap( ShopUnitFuncs.visitorInfo)
+      .flatMap(ShopUnitFuncs.visitorInfo)
       .mapPartitions(ShopUnitFuncs.filterBusinessVisitor)
       .reduceByKey((record, nextRecord) => {
-        (   record._1 ++ nextRecord._1,  record._2 ++ nextRecord._2)})
+        (record._1 ++ nextRecord._1,  record._2 ++ nextRecord._2)})
       .mapPartitions(ShopUnitFuncs.buildVisitor)
       .filter(ShopUnitFuncs.machineMacFilter)
       .cache()
+
     //history
     visitorRdd.foreachRDD(HistoryFuncs.saveHistory _)
     visitorRdd.count().map("process " + _ + " data; save data to History. " + new Date()).print
